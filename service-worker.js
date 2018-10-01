590f6e8f4f456067d16c5ac252490e33a32cf278
kriteesh <kriteesh.parashar@gmail.com>
2018-10-01 13:38:26 +0530

    new algo

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,25 @@
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('airhorner').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/sudoku.html',
+       '/js/sudoku.js',
+	   '/js/sweetAlert.min.js',
+	   '/css/animate.css',
+	   '/js/manifest.json',
+	   '/js/noSleep.js'
+     ]);
+   })
+ );
+});
+
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+ console.log(event.request.url);
+
+ event.respondWith(
+   caches.match(event.request).then(function(response) {
+     return response || fetch(event.request);
+   })
+ );
 });
