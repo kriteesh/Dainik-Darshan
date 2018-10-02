d2873d7ee3471330681e2a2999a7fa7262b118ea
kriteesh <kriteesh.parashar@gmail.com>
2018-10-02 13:39:25 +0530

    service worker fixed ?

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,19 @@
-let cacheName = 'whataboutery';
+let cacheName = 'v1';
+let cacheFiles = [
+  '/js/sudoku.js',
+  '/js/sweetAlert.min.js',
+  '/css/animate.css',
+  '/manifest.json',
+  '/sudoku.html'
+
+]
+
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(cacheName).then(function(cache) {
-      return cache.addAll(
-        [
-          '/js/sudoku.js',
-          '/js/sweetAlert.min.js',
-          '/css/animate.css',
-          '/sudoku.html',
-        ]
-      );
+      console.log(cacheName);
+      return cache.addAll(cacheFiles) ;
     })
   );
 });
@@ -20,9 +23,6 @@ self.addEventListener('activate', function(event) {
     caches.keys().then(function(cacheNames) {
       return Promise.all(
         cacheNames.filter(function(cacheName) {
-          // Return true if you want to remove this cache,
-          // but remember that caches are shared across
-          // the whole origin
         }).map(function(cacheName) {
           return caches.delete(cacheName);
         })
